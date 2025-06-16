c3e17dba57160095e3ac16c0e3e2c23326367980
josepcrespo <josep.crespo.santacreu@gmail.com>
2025-06-16 11:09:57 +0100

    Updated distribution files.
    - Updated contextMode type to return cleanup function in type definition.

--- dist/contextCursor/modes/contextMode.d.ts
+++ dist/contextCursor/modes/contextMode.d.ts
@@ -1,3 +1,3 @@
 import type { CProps } from '../types';
-declare const contextMode: (cursor: HTMLElement, props: CProps, interactElements: NodeListOf<Element>) => void;
+declare const contextMode: (cursor: HTMLElement, props: CProps, interactElements: NodeListOf<Element>) => () => void;
 export default contextMode;
